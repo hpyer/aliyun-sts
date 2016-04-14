616a0aafb587cc784743993b8b372f7ffb8aae23
Hpyer <hpyer@misscandy.com>
2016-04-14 16:32:33 +0800

    init

---
+++ config/aliyun_sts.php
@@ -0,0 +1,30 @@
+<?php
+return [
+
+	// AccessKeyID
+	'key' : env('ALIYUN_STS_KEY'),
+
+	// AccessKeySecret
+	'secret' : env('ALIYUN_STS_SECRET'),
+
+	// RoleArn
+	'role_arn' : env('ALIYUN_STS_ROLE_ARN'),
+
+	// Token的失效时间，最少是900s
+	'expire_time' : env('ALIYUN_STS_EXPIRE_TIME', 900),
+
+	// Token所要拥有的权限
+	'policy' : {
+		"Statement": [
+			{
+				"Action": [
+					"oss:*"
+				],
+				"Effect": "Allow",
+				"Resource": ["acs:oss:*:*:*"]
+			}
+		],
+		"Version": "1"
+	},
+
+];
---
+++ src/Provider/AliyunStsServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+namespace Hpyer\Aliyun\Sts\Provider;
+
+use RuntimeException;
+use Illuminate\Support\ServiceProvider;
+
+class AliyunStsServiceProvider extends ServiceProvider
+{
+	/**
+	 * Boot the service provider.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->mergeConfigFrom(realpath(__DIR__.'/../../config/aliyun_sts.php'), 'aliyun_sts');
+
+		$config = $this->app['config']['aliyun_sts'];
+
+		if (! $this->app->runningInConsole() && (empty($config['key']) || empty($config['secret']) || empty($config['role_arn']))) {
+			throw new RuntimeException('Unable to boot AliyunStsServiceProvider, please set key, secret, role_arn in `config/aliyun_sts.php` file.');
+		}
+	}
+
+}
