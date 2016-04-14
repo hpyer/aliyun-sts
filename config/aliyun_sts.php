e37d86404ebc75e9e5af4e77a0e7ddf09c977180
Hpyer <hpyer@misscandy.com>
2016-04-14 17:12:52 +0800

    fix

--- config/aliyun_sts.php
+++ config/aliyun_sts.php
@@ -2,29 +2,29 @@
 return [
 
 	// AccessKeyID
-	'key' : env('ALIYUN_STS_KEY'),
+	'key' => env('ALIYUN_STS_KEY'),
 
 	// AccessKeySecret
-	'secret' : env('ALIYUN_STS_SECRET'),
+	'secret' => env('ALIYUN_STS_SECRET'),
 
 	// RoleArn
-	'role_arn' : env('ALIYUN_STS_ROLE_ARN'),
+	'role_arn' => env('ALIYUN_STS_ROLE_ARN'),
 
 	// Token的失效时间，最少是900s
-	'expire_time' : env('ALIYUN_STS_EXPIRE_TIME', 900),
+	'expire_time' => env('ALIYUN_STS_EXPIRE_TIME', 900),
 
 	// Token所要拥有的权限
-	'policy' : {
-		"Statement": [
+	'policy' => [
+		"Statement" => [
 			{
-				"Action": [
+				"Action" => [
 					"oss:*"
 				],
-				"Effect": "Allow",
-				"Resource": ["acs:oss:*:*:*"]
+				"Effect" => "Allow",
+				"Resource" => ["acs:oss:*:*:*"]
 			}
 		],
-		"Version": "1"
-	},
+		"Version" => "1"
+	],
 
 ];
